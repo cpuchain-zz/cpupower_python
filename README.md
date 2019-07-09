652a31478ca7112c93c0641f12459b15e320f1e3
Min Khang Aung <minkcrypto@gmail.com>
2019-07-09 17:35:52 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# cpupower-python
+
+[![PyPI version](https://badge.fury.io/py/cpupower.svg)](https://badge.fury.io/py/cpupower)
+[![Build status](https://ci.appveyor.com/api/projects/status/it86j7ypo815o83l/branch/master?svg=true)](https://ci.appveyor.com/project/minkcrypto/cpupower-python/branch/master)
+
+This package implements the hashing algorithm used by CPUchain.
+
+## Install
+
+```bash
+$ pip install cpupower
+```
+
+## Usage
+
+```python
+    import cpupower
+```
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Extension
+
+cpupower_module = Extension('cpupower',
+                            sources = ['yespower-module.c',
+                                       'yespower.c',
+                                       'yespower-opt.c',
+                                       'sha256.c'
+                                       ],
+                            extra_compile_args=['-O2', '-funroll-loops', '-fomit-frame-pointer'],
+                            include_dirs=['.'])
+
+setup (name = 'cpupower',
+       version = '1.0.0',
+       author_email = 'minkcrypto@gmail.com',
+       author = 'Min Khang Aung',
+       url = 'https://github.com/cpuchain/cpupower_python',
+       description = 'Bindings for CPUpower proof of work used by CPUchain',
+       ext_modules = [cpupower_module])
